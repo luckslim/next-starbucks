9939c7ac299a584901932135785fb313611a8d6c
lucaslima <lucaslima78@hotmail.com>
2025-04-28 07:40:14 -0300

    up

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,8 +1,34 @@
+import { PrismaClient } from '@prisma/client'
 import { NextRequest, NextResponse } from 'next/server'
+interface Product{
+  description:string
+  quantity: number
+  total:number
+}
+interface OrderData {
+  username:string
+  email:string
+  product: Product[]
+  total: number
+}
 export async function POST(req: NextRequest) {
-  const data = await req.json()
+  const prisma = new  PrismaClient()
+  const data = (await req.json()) as OrderData
+  const {username,email}= data
+  const order = await prisma.orders.create({
+    data:{
+      name: username,
+      email: email
+    }
+  })
   return NextResponse.json({
     message: 'Ordem recebida com sucesso!',
-    data,
+    // data:{
+    //   username,
+    //   email,
+    //   product,
+    //   total
+    // },
+    order
   })
 }
